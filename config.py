14c0e74facc58c88cb922659729a1365cab23e16
Danifeeerr <danielferp2004@gmail.com>
2026-04-16 17:23:29 +0200

    minor fixes

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
+import sys
 from dotenv import load_dotenv
 
-load_dotenv()
+if getattr(sys, 'frozen', False):
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
+load_dotenv(os.path.join(base_path, ".env"))
 API_BASE = os.getenv("API_BASE", "http://localhost:8000")
